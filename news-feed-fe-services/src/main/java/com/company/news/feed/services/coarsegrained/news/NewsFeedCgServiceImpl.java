d45ae15dc2d08fe11df53166fabd7a7a2216dc6a
artem.isaenok <artem.isaenok@accenture.com>
2016-10-07 11:44:02 +0300

    Added annotation @Autowired.

--- news-feed-fe-services/src/main/java/com/company/news/feed/services/coarsegrained/news/NewsFeedCgServiceImpl.java
+++ news-feed-fe-services/src/main/java/com/company/news/feed/services/coarsegrained/news/NewsFeedCgServiceImpl.java
@@ -4,12 +4,15 @@ import java.util.List;
 
 import javax.jws.WebService;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.company.news.feed.services.dto.news.ArticleDto;
 import com.company.news.feed.services.finegrained.news.NewsFeedFgService;
 
 @WebService(endpointInterface = "com.company.news.feed.services.coarsegrained.news.NewsFeedCgService")
 public class NewsFeedCgServiceImpl implements NewsFeedCgService {
 	
+	@Autowired
 	private NewsFeedFgService newsFeedFgService;
 
 	public List<ArticleDto> getLastNews() {
