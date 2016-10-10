8266afe8e5c5c7a60ea6a0989e33976a04208118
artem.isaenok <artem.isaenok@accenture.com>
2016-10-10 14:11:49 +0300

    Added SOAP service NewsFeedCgService.

--- news-feed-bh-services/src/main/java/com/company/news/feed/referencial/business/news/NewsFeedBusinessServiceImpl.java
+++ news-feed-bh-services/src/main/java/com/company/news/feed/referencial/business/news/NewsFeedBusinessServiceImpl.java
@@ -3,10 +3,12 @@ package com.company.news.feed.referencial.business.news;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.company.news.feed.referencial.access.news.NewsRepository;
 import com.company.news.feed.referencial.entity.news.Article;
 
+@Service
 public class NewsFeedBusinessServiceImpl implements NewsFeedBusinessService {
 	
 	@Autowired
--- news-feed-fe-services/src/main/java/com/company/news/feed/services/finegrained/news/NewsFeedFgServiceImpl.java
+++ news-feed-fe-services/src/main/java/com/company/news/feed/services/finegrained/news/NewsFeedFgServiceImpl.java
@@ -3,10 +3,12 @@ package com.company.news.feed.services.finegrained.news;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.company.news.feed.referencial.business.news.NewsFeedBusinessService;
 import com.company.news.feed.services.dto.news.ArticleDto;
 
+@Service
 public class NewsFeedFgServiceImpl implements NewsFeedFgService {
 	
 	@Autowired
